8cea4562de4b74d1d86a5ad58d71d32730396a54
DanielMilstein <dnmilstein@miuandes.cl>
2021-04-06 21:18:55 -0400

    Funcion para modo de juego

--- tarea.c
+++ tarea.c
@@ -2,11 +2,28 @@
 #include <stdlib.h>
 #include <string.h>
 
+int tipo_juego(char tipo[2]){
+	if (tipo[1] == 'a') return 0;
+	else
+		if (tipo[1] == 'v') return 1;
+	else return 9;
+
+}
 
 
-
-int main(int argc, char const *argv[])
+int main(int argc, char** argv)
 {
-	printf("%s\n", argv[1]);
+	//Si no se le pasan los argumentos al programa, termina solo.
+	if (argc<4){
+		printf("Faltan argumentos\n");
+		return 0;
+	}
+
+	printf("%s\n", argv[0]);
+
+	
+
+
+	
 	return 0;
 }
