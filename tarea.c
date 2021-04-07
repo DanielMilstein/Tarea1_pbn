a5248d5c1b1e665d0267a8d8d6d289b1c5e72dea
DanielMilstein <dnmilstein@miuandes.cl>
2021-04-06 22:34:17 -0400

    Empezando parse_tablero

--- tarea.c
+++ tarea.c
@@ -2,6 +2,9 @@
 #include <stdlib.h>
 #include <string.h>
 
+//1 -> true 
+//0 -> false
+
 int tipo_juego(char tipo[2]){
 	if (tipo[1] == 'a') return 0;
 	else
@@ -11,24 +14,50 @@ int tipo_juego(char tipo[2]){
 }
 
 
+void parse_tablero(char* nombre_archivo){
+
+	FILE* archivo;
+	archivo = fopen(nombre_archivo, "r");
+
+
+
+
+
+	fclose(archivo);
+	return;
+
+}
+
+
 int main(int argc, char** argv)
 {
-<<<<<<< HEAD
+
 	//Si no se le pasan los argumentos al programa, termina solo.
 	if (argc<4){
 		printf("Faltan argumentos\n");
 		return 0;
 	}
 
-	printf("%s\n", argv[0]);
-
-	
 
 
-	
-=======
-	printf("%s\n", argv[1]);
-	printf("Hola\n");
->>>>>>> fd450ed106a30be88177e0670f8164222ea1aca8
+	//Si es 1 (true) el modo es versus
+	if (tipo_juego(argv[1])){
+
+
+
+
+	}
+
+	else //Si es 0 el modo es automatizado
+		if (tipo_juego(argv[1]) == 0){
+
+
+
+
+		}
+
+
+	else printf("Modo de juego no valido.\n");
+
 	return 0;
 }
