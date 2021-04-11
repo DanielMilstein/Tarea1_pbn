7b34ad891f1ab7d30d3ef83467b58d9b65b8f0d9
DanielMilstein <dnmilstein@miuandes.cl>
2021-04-11 04:49:01 -0400

    Agregue funcion separada para imprimir el tablero, y ahora se imprime sin meter las letras y numeros al array. El array ahora tambien es solo de chars _ para facilitar trabajar con eso despues. Tambien meti un script para compilar mas facil.

--- tarea.c
+++ tarea.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#define GRID_SIZE 11
+#define GRID_SIZE 10
 
 //1 -> true 
 //0 -> false
@@ -16,19 +16,25 @@ int tipo_juego(char tipo[2]){
 
 char *arr[GRID_SIZE][GRID_SIZE], *datos[100], dato[100];
 
-void parse_tablero(char* nombre_archivo){
+void parse_tablero(char* nombre){
 
+	char nombre_archivo[15];
+	sprintf(nombre_archivo, "./%s.txt", nombre);
+	printf("%s\n", nombre_archivo);
 	FILE* archivo;
+	
 	archivo = fopen(nombre_archivo, "r");
 
- 	if (archivo == NULL) {
-    		printf("No se pudo abrir el archivo");
-    	} else {
+ 	if (0) {
+    		printf("No se pudo abrir el archivo\n");
+    	} 
+    else {
   		int i=0;
   		while(!feof(archivo)){
+
  			fscanf(archivo, "%s", dato);
  			datos[i] = dato;
- 			printf("%s", datos[i]);
+ 			printf("%s\n", datos[i]);
  			i++;
 			}
 		
@@ -41,13 +47,13 @@ void parse_tablero(char* nombre_archivo){
 
 
 
-int board(void) {
+void board() {
     for (int a = 0; a < GRID_SIZE; a++) {
         for (int b = 0; b < GRID_SIZE; b++) {
-		arr[a][b] = "[_]";
+			arr[a][b] = "[_]";
         }
     }
-	arr[0][0]= " ";
+/*	arr[0][0]= " ";
 	arr[1][0]= "0";
 	arr[2][0]= "1";
 	arr[3][0]= "2";
@@ -58,14 +64,14 @@ int board(void) {
 	arr[8][0]= "7";
 	arr[9][0]= "8";
 	arr[10][0]= "9";
-	/*
+	
 	char x;
 	for(int i=1; i< GRID_SIZE-1; i++){
 		x= i + '0';
 		arr[i][0]= x;
 
 		}
-		*/
+		
 		
 	arr[0][1]= " a ";
 	arr[0][2]= " b ";
@@ -77,7 +83,7 @@ int board(void) {
 	arr[0][8]= " h ";
 	arr[0][9]= " i ";
 	arr[0][10]=" j ";
-
+*/
 	
 	
     printf("\n");
@@ -90,38 +96,67 @@ int board(void) {
     }
 }
 
+void crear_tablero() {
+    for (int a = 0; a < GRID_SIZE; a++) {
+        for (int b = 0; b < GRID_SIZE; b++) {
+			arr[a][b] = "[_]";
+        }
+    }
+}
+
+
+
+
+void print_tablero(char** array){
+
+	printf("   A  B  C  D  E  F  G  H  I  J\n");
+	for (int a = 0; a < GRID_SIZE; a++) {
+		printf("%d ", a);
+		
+	    for (int b = 0; b < GRID_SIZE; b++) {
+			printf("[%c]", array[a][b]);
+	    }
+	    printf(" %d", a);
+	    printf("\n");
+	}
+	printf("   A  B  C  D  E  F  G  H  I  J\n");
+}
+
+
+
+
 int main(int argc, char** argv)
 {
-	board();
+	//board();
 	
-	parse_tablero(argv);
-	
-	//Si no se le pasan los argumentos al programa, termina solo.
-	if (argc<4){
-		printf("Faltan argumentos\n");
-		return 0;
-	}
+	parse_tablero(argv[1]);
+
+	// //Si no se le pasan los argumentos al programa, termina solo.
+	// if (argc<4){
+	// 	printf("Faltan argumentos\n");
+	// 	return 0;
+	// }
 
 
 
-	//Si es 1 (true) el modo es versus
-	if (tipo_juego(argv[1])){
-
-
-
-
-	}
-
-	else //Si es 0 el modo es automatizado
-		if (tipo_juego(argv[1]) == 0){
+	// //Si es 1 (true) el modo es versus
+	// if (tipo_juego(argv[1])){
 
 
 
 
-		}
+	// }
+
+	// else //Si es 0 el modo es automatizado
+	// 	if (tipo_juego(argv[1]) == 0){
 
 
-	else printf("Modo de juego no valido.\n");
+
+
+	// 	}
+
+
+	// else printf("Modo de juego no valido.\n");
 
 	return 0;
 }
